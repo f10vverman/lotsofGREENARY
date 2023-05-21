228b02a1d913edc2f5f26a6841e606b57dc41b46
f10vverman <102749200+f10vverman@users.noreply.github.com>
2023-05-22 02:01:49 +0400

    finish gulp

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+const gulp = require('gulp'),
+    sass = require('gulp-sass') (require('sass')),
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    cssmin = require('gulp-cssmin');
+
+
+gulp.task('sass', function(){
+    return gulp.src('app/scss/**/*.scss')
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(autoprefixer({
+        overrideBrowserslist: ['last 8 versions']
+    }))
+    .pipe(gulp.dest('app/css'))
+    .pipe (browserSync.reload({stream: true}))
+});
+gulp.task('style', function(){
+    return gulp.src([
+        "node_modules/normalize.css/normalize.css",
+        "node_modules/slick-carousel/slick/slick.css",
+        "node_modules/magnific-popup/dist/magnific-popup.css",
+        "node_modules/jquery-form-styler/dist/jquery.formstyler.css",
+        "node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css"
+    ])
+    .pipe(concat("libs.min.css"))
+    .pipe(cssmin())
+    .pipe(gulp.dest("app/css"))
+});
+gulp.task('script', function(){
+    return gulp.src([
+        "node_modules/slick-carousel/slick/slick.js",
+        "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
+        "node_modules/jquery-form-styler/dist/jquery.formstyler.js",
+    ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'))
+});
+gulp.task("html", function() {
+    return gulp.src('app/*.html')
+    .pipe(browserSync.reload({stream: true}));
+});
+gulp.task("js", function(){
+    return gulp.src('app/js/*.js')
+    .pipe(browserSync.reload({stream: true}));
+});
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app/'
+        }
+    });
+});
+gulp.task('export', function() {
+    let buildHtml = gulp.src('app/**/*.html').pipe(gulp.dest('build'));
+    let buildCSS = gulp.src('app/css/*.css').pipe(gulp.dest('build/css'));
+    let buildJs = gulp.src('app/js/**/*.js').pipe(gulp.dest('build/js'));
+    let buildFonts = gulp.src('app/fonts/**/*.*').pipe(gulp.dest('build/fonts'));
+    let buildImages = gulp.src('app/images/**/*.*').pipe(gulp.dest('build/images'));
+});
+gulp.task('watch', function(){
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
+    gulp.watch('app/*.html', gulp.parallel('html'))
+    gulp.watch('app/js/*.js', gulp.parallel('js'))
+});
+gulp.task('build', gulp.series('sass', 'export'));
+gulp.task('default', gulp.parallel('style', 'script', 'sass', 'watch', 'browser-sync'));
